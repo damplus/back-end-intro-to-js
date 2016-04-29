7322730a7e906739d6c21d2602789143cf7ae282
Angus Goldsmith <angus@bright-interactive.co.uk>
2016-04-29 15:36:56 +0100

    Added some comments, added watch command.

--- part-one/src/index.js
+++ part-one/src/index.js
@@ -1,18 +1,29 @@
+//ES6 class syntax
 class Counter {
-    constructor(initialValue = 0) {
+    //destructuring assignment and default values
+    constructor({initialValue = 0, increment = 1, name}) {
         this.count = initialValue;
+        this.incrementAmount = increment;
+        this.name = name;
     }
 
     increment() {
-        this.count++;
+        this.count += this.incrementAmount;
     }
 
     decrement() {
-        this.count--;
+        this.count -= this.incrementAmount;
     }
 
     isGreaterThan(number) {
         return this.count > number;
+    }
+
+    toString() {
+        //destructuring assignment.  Equivalent to const name = this.name; const count = this.count;
+        const {name, count} = this;
+        //string interpolation
+        return `Counter named ${name} with value ${count}`;
     }
 }
 
@@ -20,11 +31,28 @@ function incrementAllCounters(counters) {
     return counters.forEach(counter => counter.increment());
 }
 
-const counters = [new Counter(2), new Counter()];
+function printCounter(counter) {
+    console.log(counter.toString());
+}
+
+//const defines a constant
+const counterConfigs = [
+    {
+        initialValue: 2,
+        name: "Krombopulos"
+    },
+    {
+        initialValue: 0,
+        increment: 2,
+        name: "Bird Person"
+    }
+];
+
+//Using a lambda in map
+const counters = counterConfigs.map(counterConfig => new Counter(counterConfig));
 
 incrementAllCounters(counters);
 
 const countersAboveTwo = counters.filter(counter => counter.isGreaterThan(2));
 
-console.log(countersAboveTwo);
-
+countersAboveTwo.forEach(printCounter);
